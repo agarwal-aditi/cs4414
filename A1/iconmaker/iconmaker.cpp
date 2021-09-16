928660545cd63475eff3f509dbfcb1cf837d3f27
agarwal-aditi <aa2224@cornell.edu>
2021-09-15 22:34:45 -0400

    iconmaker works

--- A1/iconmaker/iconmaker.cpp
+++ A1/iconmaker/iconmaker.cpp
@@ -32,7 +32,7 @@ void lr_diag(pngwriter &png, string &color){
 
 }
 
-void rl_diag(pngwriter &png, string &color){
+void diag_one(pngwriter &png, string &color){
     double colors[3] = {0.0,0.0,0.0};
     
     if(!color.compare("r")) colors[0] = 1.0;
@@ -41,13 +41,27 @@ void rl_diag(pngwriter &png, string &color){
         colors[1] = 1.0;
     }
     else if(!color.compare("g")) colors[1] = 1.0;
-    //pick up here
-    png.line(1,1,64,64,colors[0],colors[1],colors[2]);
+    png.line(1,64,64,1,colors[0],colors[1],colors[2]);
     for(int i = 1; i < 4; i++){
-        png.line(1,1+i,64-i,1,colors[0],colors[1],colors[2]);
+        png.line(1,64-i,64-i,1,colors[0],colors[1],colors[2]);
         png.line(1+i,64,64,1+i,colors[0],colors[1],colors[2]);
     }
+}
 
+void diag_two(pngwriter &png, string &color){
+    double colors[3] = {0.0,0.0,0.0};
+    
+    if(!color.compare("r")) colors[0] = 1.0;
+    else if(!color.compare("y")){
+        colors[0] = 1.0;
+        colors[1] = 1.0;
+    }
+    else if(!color.compare("g")) colors[1] = 1.0;
+    png.line(1,1,64,64,colors[0],colors[1],colors[2]);
+    for(int i = 1; i < 4; i++){
+        png.line(1,1+i,64-i,64,colors[0],colors[1],colors[2]);
+        png.line(1+i,1,64,64-i,colors[0],colors[1],colors[2]);
+    }
 }
 
 int main()
@@ -65,13 +79,26 @@ int main()
             vert_line(png,colors[j]);
             png.close();
 
-            // for(k=0; k<colors.size();k++){
-            //     //3 streets
-            //     string file_path = "./files/i3"+colors[i]+colors[j]+colors[k]".png";
-            //     pngwriter png(64,64,65535,file_path.c_str());
-            //     horiz_line(png,colors[i]);
-                
-            // }
+            for(auto k=0; k<colors.size();k++){
+                //3 streets
+                string file_path = "./files/i3"+colors[i]+colors[j]+colors[k]+".png";
+                pngwriter png(64,64,65535,file_path.c_str());
+                horiz_line(png,colors[i]);
+                diag_one(png,colors[j]);
+                diag_two(png,colors[k]);
+                png.close();
+
+                for(auto m=0; m<colors.size();m++){
+                    //4 streets
+                    string file_path = "./files/i4"+colors[i]+colors[j]+colors[k]+colors[m]+".png";
+                    pngwriter png(64,64,65535,file_path.c_str());
+                    horiz_line(png,colors[i]);
+                    diag_one(png,colors[j]);
+                    vert_line(png,colors[k]);
+                    diag_two(png,colors[m]);
+                    png.close();
+                }
+            }
         }
     }
 
